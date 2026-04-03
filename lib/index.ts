1e4e70356ba2d38ee2a5016b67a7b887b48af127
emifirpo <61640027+emifirpo@users.noreply.github.com>
2026-04-03 15:03:22 -0300

    chore: add lib/index.ts

---
+++ lib/index.ts
@@ -0,0 +1,17 @@
+// Types
+export type {
+  BrandbookSchema,
+  BrandColor,
+  Typography,
+} from "./types";
+
+// Utils
+export { cn } from "./utils";
+
+// Constants
+export {
+  GENERATION_STEPS,
+  GENERATION_STEP_DURATION_MS,
+  MAX_REFERENCE_IMAGES,
+  MIN_PROMPT_LENGTH,
+} from "./constants";

ac0f340f4ed43bb50ae6e60e8876dd80dc8747aa
emifirpo <61640027+emifirpo@users.noreply.github.com>
2026-04-03 14:51:24 -0300

    chore: add components/SectionEditor.tsx

---
+++ components/SectionEditor.tsx
@@ -0,0 +1,5 @@
+/**
+ * @deprecated Moved to @/components/brandbook/SectionEditor
+ */
+export { SectionEditor } from "./brandbook/SectionEditor";
+export type { SectionContent } from "./brandbook/SectionEditor";
